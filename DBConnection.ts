1b86495e201a40d3c8f2875331dca004da771262
nandini <jitcse.ndeshpande2001@gmail.com>
2024-05-05 16:57:01 +0530

    Initial commit

---
+++ DBConnection.ts
@@ -0,0 +1,19 @@
+import {DataSource} from 'typeorm';
+import dotev from "dotenv";
+
+dotev.config();
+
+export const dbConnection=new DataSource({
+
+    type:"mysql",
+    host:"todolist.cpygkmakwokt.eu-north-1.rds.amazonaws.com",
+    port:3306,
+    password:"pritiraju",
+    username:"root",
+    database:"node_todo_list",
+    synchronize:true,
+    entities:[
+       './Entity/*.ts'
+    ]
+})
+
